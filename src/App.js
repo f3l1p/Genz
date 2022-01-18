1c014e4132c9d34e706c6f83674f18b8a6da1f82
Felipe <wnrad.f@gmail.com>
2022-01-18 13:31:35 -0500

    arquitecture-update

--- src/App.js
+++ src/App.js
@@ -1,26 +1,26 @@
-import logo from "./logo.svg";
+import React from "react";
 import "./App.css";
 import AOS from "aos";
 import "aos/dist/aos.css";
 AOS.init();
+//COMPONENTS
+//card, navbar, footer => giovanni
+// title, button => felipe
 
 function App() {
 	return (
-		<div className="App">
-			<header className="App-header">
-				<img src={logo} className="App-logo" alt="logo" />
-				<p>
-					Edit <code>src/App.js</code> and save to reload.
-				</p>
-				<a
-					className="App-link"
-					href="https://reactjs.org"
-					target="_blank"
-					rel="noopener noreferrer"
-				>
-					Learn React
-				</a>
-			</header>
+		<div>
+			<h1>Ready?</h1>
+			{/* 
+			<Navbar/> => giovanni
+			<Home/> => Felipe
+			<About/> => Felipe / Giovanni las cards
+			<WhyGenz/> => Felipe
+			<Solutions /> => Giovanni
+			<Process/> => Felipe / Giovanni las cards
+			<Tools/> => Giovanni
+			<Contact/> => Felipe // queda pendiente el form.
+			 */}
 		</div>
 	);
 }
---
+++ src/components/TitleSection/TitleSection.js
@@ -0,0 +1,9 @@
+import React from "react";
+import "./TitleSection.scss";
+
+// Titulo de las secciones
+export const TitleSection = ({ title }) => (
+	<div className="title-container">
+		<h2 className="title-name">{title}</h2>
+	</div>
+);
