30d6ea7f01e78fe16181029e03e0736ab84adbaa
Felipe <wnrad.f@gmail.com>
2022-02-14 11:43:43 -0500

    react-helmet-implementation

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import "./scss/index.scss";
 import AOS from "aos";
 import "aos/dist/aos.css";
 
+import { Helmet } from "react-helmet";
+
 import { Home } from "./sections/Home/Home";
 import { About } from "./sections/About/About";
 import { WhyGenz } from "./sections/WhyGenz/WhyGenz";
@@ -15,19 +17,27 @@ import { Contact } from "./sections/Contact/Contact";
 AOS.init();
 
 function App() {
-  return (
-    <>
-      <NavBar />
-      <Home />
-      <About />
-      <WhyGenz />
-      <Solution />
-      <Process />
-      <Tools />
-      <Contact />
-      <Footer />
-    </>
-  );
+	return (
+		<>
+			<Helmet>
+				<title>Genz</title>
+				<meta name="description" content="Soluciones para tu empresa" />
+				<meta
+					name="keywords"
+					content="genz, soluciones, inovacion, tecnologia"
+				/>
+			</Helmet>
+			<NavBar />
+			<Home />
+			<About />
+			<WhyGenz />
+			<Solution />
+			<Process />
+			<Tools />
+			<Contact />
+			<Footer />
+		</>
+	);
 }
 
 export default App;
